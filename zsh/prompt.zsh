6d6f973b0799a2feebe63a8c5b14703c81936f9d
Erik Davidson <erik@erikd.org>
2012-10-24 00:44:21 -0500

    Update to my own prefs

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -40,6 +40,10 @@ need_push () {
   fi
 }
 
+who_prompt(){
+  echo "$(whoami)@$(hostname)"
+}
+
 rb_prompt(){
   if $(which rbenv &> /dev/null)
   then
@@ -72,7 +76,7 @@ directory_name(){
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(rb_prompt) in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'\n$(who_prompt) in $(directory_name) $(git_dirty)$(need_push)\n$ '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
 }
