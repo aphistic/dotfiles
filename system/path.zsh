d67bc2ed1413ab2481a6518f3bb2d7fb66a2ac2a
Erik Davidson <erik@erikd.org>
2013-05-11 15:33:26 -0500

    Updates to sublime packages and go-related paths.

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,5 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/go/bin:$PATH"
+export GOPATH="$HOME/godev"
+export GOROOT="$HOME/go"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
