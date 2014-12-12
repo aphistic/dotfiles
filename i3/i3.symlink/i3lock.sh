2c28da2e2f997441e6ce41f5855757d689b4e2bf
Erik Davidson <erik.davidson@corvisa.com>
2014-12-12 13:31:24 -0600

    A bunch of stuff from work

--- i3/i3.symlink/i3lock.sh
+++ i3/i3.symlink/i3lock.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 scrot /tmp/screen_locked.png
-convert /tmp/screen_locked.png -blur 5x5 /tmp/screen_locked.png
+convert /tmp/screen_locked.png -blur 7x7 /tmp/screen_locked.png
 i3lock -i /tmp/screen_locked.png
