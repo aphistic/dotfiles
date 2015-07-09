5bf4e6ba7a9ffc413a3fbeed7bfeb1c7993b5abb
Erik Davidson <erik@erikd.org>
2015-07-09 12:20:00 -0500

    Update for debian testing. Allow default files so it doesn't need a config for each host

--- script/platform.py
+++ script/platform.py
@@ -2,7 +2,8 @@ import os
 
 
 def detect_platform():
-    if os.path.isfile('/etc/debian_release'):
+    if os.path.isfile('/etc/debian_release') or \
+       os.path.isfile('/etc/debian_version'):
         return 'linux', 'debian'
     elif os.path.isfile('/etc/arch-release'):
         return 'linux', 'arch'
