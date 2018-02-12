b32cfafed246b83dd1edee0f9d53cdc7544c0405
Erik Davidson <erik@erikd.org>
2018-02-12 01:01:29 -0600

    Update for better hidpi support on akame

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,12 +6,21 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+{{ if eq .Env.HM_TASK "akame" }}
+
+polybar first &
+
+{{ else if eq .Env.HM_TASK "aphng" }}
+
 polybar first &
 polybar second &
 polybar third &
-
-{{ if eq .Env.HM_TASK "aphng" }}
-
 polybar fourth &
+
+{{ else if eq .Env.HM_TASK "ace" }}
+
+polybar first &
+polybar second &
+polybar third &
 
 {{ end }}
