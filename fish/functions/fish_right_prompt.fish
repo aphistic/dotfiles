5bb569962111281fe1704a861dbef93c59619384
Kristin Davidson <kdavidson@atlassian.com>
2020-06-17 09:48:56 -0500

    Fix am/pm in right prompt on macos

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,3 +1,3 @@
 function fish_right_prompt
-    echo -n -s (set_color $fish_color_autosuggestion) (date +"%Y-%m-%d %I:%M:%S%P") (set_color reset)
+    echo -n -s (set_color $fish_color_autosuggestion) (date +"%Y-%m-%d %I:%M:%S%p") (set_color reset)
 end
