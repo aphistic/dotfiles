edbe4bc7cc605e8d6710586ede6ee8f1e8303081
Kristin Davidson <k@kxd.dev>
2020-07-20 21:17:31 -0500

    Add fate spotify config

--- spotify/spotify.sh
+++ spotify/spotify.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-
-{{ if eq .Env.HM_TASK "kxdatlas" }}
+{{ if or (eq .Env.HM_TASK "kxdatlas") (eq .Env.HM_TASK "fate") }}
 /usr/bin/spotify --force-device-scale-factor=1.75
 {{ else }}
 /usr/bin/spotify
