de965e3dffffb690c66cec311ca477f8e5778ed1
Kristin Davidson <kdavidson@atlassian.com>
2020-11-13 15:54:53 -0600

    Add funcs to make switching git repo configs easier

--- git/git.fish
+++ git/git.fish
@@ -8,3 +8,13 @@ abbr gp 'git push'
 abbr gpl 'git pull'
 abbr gs 'git status'
 abbr gl 'git lg'
+
+function gu_me
+    git config user.name "Kristin Davidson"
+    git config user.email "k@kxd.dev"
+end
+
+function gu_work
+    git config user.name "Kristin Davidson"
+    git config user.email "kdavidson@atlassian.com"
+end
