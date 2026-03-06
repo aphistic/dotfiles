054a0da79488f499fd9d47a6c5cc2e54fca500f2
Kristin Davidson <vcs@k.kxd.dev>
2026-03-05 18:02:10 -0600

    set up more fnox utility stuff

--- dot_config/mise/tasks/fnox/executable_login.sh
+++ dot_config/mise/tasks/fnox/executable_login.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 #MISE description="Perform login required for fnox"
 
-sa_name="fnox-$(hostname)"
 
+echo "Setting up 1Password"
 set -e
+sa_name="fnox-$(hostname)"
 OP_TOKEN="$(op service-account create "${sa_name}" --raw --vault infra-common:read_items)"
 fnox set OP_SERVICE_ACCOUNT_TOKEN "${OP_TOKEN}" --provider age
+set +e
+
+echo "Setting up Bitwarden"
+bw login
+set -e
+BW_SESSION="$(bw unlock --raw)"
+fnox set BW_SESSION "${BW_SESSION}" --provider age
 set +e
